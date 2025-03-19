847ed2d4a33d4caf9b527fd4e8c2cd439eca21f4
supriyob <supriyob@gmail.com>
2025-03-19 12:37:10 +0600

    Added a Student.java file in task 1 folder

---
+++ LabTask1/Student.java
@@ -0,0 +1,12 @@
+ /*
+ * Developing basic Java classes with attributes and methods.
+ * Lab task - a 
+ */
+ import java.lang.*;
+ public class Student{
+    private int id;
+    public static void main(){
+
+    }
+ }
+ 
