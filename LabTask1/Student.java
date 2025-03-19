e2c6855bb5a51f7e4fd95bcd6764d473883656d5
supriyob <supriyob@gmail.com>
2025-03-19 12:57:37 +0600

    Task-1 completed

--- LabTask1/Student.java
+++ LabTask1/Student.java
@@ -1,12 +1,46 @@
  /*
+ * 24-59312-3
  * Developing basic Java classes with attributes and methods.
- * Lab task - a 
+ * LabTask - a
  */
  import java.lang.*;
  public class Student{
     private int id;
-    public static void main(){
+    private String name;
+    private double cgpa;
+    // Default constructor
+    public Student(){
 
+    }
+    // Set method
+    public void setId(int id){
+      this.id = id;
+    }
+    public void setName(String name){
+      this.name = name;
+    }
+    public void setCgpa(double cgpa){
+      this.cgpa = cgpa;
+    }
+    // Get method
+    public int getId(){
+      return id;
+    }
+    public String getName(){
+     return name;
+    }
+    public double getCgpa(){
+      return cgpa;
+    }
+    // Main function
+    public static void main(String[] args){
+      Student obj1 = new Student();
+      obj1.setName("Supriyo");
+      obj1.setId(24);
+      obj1.setCgpa(4.0);
+      System.out.println("Name : " + obj1.name);
+      System.out.println("Name : " + obj1.id);
+      System.out.println("Name : " + obj1.cgpa);
     }
  }
  
